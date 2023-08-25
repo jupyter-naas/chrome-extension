ae6bdbea39ca59119067981feccdaab08d8c9bdd
Hassan <ghassanpk@gmail.com>
2023-08-25 10:20:54 +0500

    new setup with react and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#47dd82",
+        secondary: "#181a1c",
+      },
+    },
+  },
+  plugins: [],
+};
